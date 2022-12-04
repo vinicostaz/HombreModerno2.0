c7ec8cf7daf4f6ac5fa2e8105fe8435723212511
Rafael Holanda <113954195+Rafael-FHP@users.noreply.github.com>
2022-12-04 22:35:32 +0000

    Adição da rota de Compra, porém com alguns defeitos

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-
 const express = require('express');
 const fs = require('fs');
 
 const app = express();
+const bodyparser = require('body-parser');
+app.use(bodyparser.urlencoded({extended: true}));
 
 let html = fs.readFileSync('index.html');
-
+app.get('/comprar', (req, res) => {
+  res.sendFile(__dirname + "/comprar.html");
+});
+app.post('/comprar', (req, res) => {
+  const val1 = Number(req.body.val1);
+  if(val1 == "Norte"){
+    val1 = 15;
+  }else if(val1 == "Nordeste"){
+    val1 = 13;
+  }else if(val1 == "Centro Oeste"){
+    val1 = 18;
+  }else if(val1 == "Sudeste"){
+    val1 = 20;
+  }else if(val1 == "Sul"){
+    val1 = 22;
+  }
+  const val2 = Number(req.body.val2);
+  if(val2 == "Bermuda Branco" || "Bermuda Bege" || "Bermuda Preta"){
+    val2 = 69,99;
+  }else if(val2 == "Camisa Azul" || "Camisa Vermelha" || "Camisa Verde"){
+    val2 = 59,99;
+  }else if(val2 == "Calça Marrom" || "Jean Azul" || "Jeans Preta"){
+    val2 = 89,99;
+  }
+  const soma = val1 + val2;
+  res.send("O seu valor de compra final é: " + soma);
+});  
 app.get('/', (req, res) => {
   res.send(html)
 });
@@ -34,9 +61,6 @@ app.get('/QS', (req, res) => {
   res.end(fs.readFileSync('QS.html'));
 })
 
-app.get('/comprar', (req, res) => {
-  res.end(fs.readFileSync('comprar.html'));
-})
 
 app.get('/index', (req, res) => {
   res.end(fs.readFileSync('index.html'));
